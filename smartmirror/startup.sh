7cf5caa0bafb307ddcaa86eefcca34d4e05d6c89
Sören Gutzeit <soeren.gutzeit@gmail.com>
2017-05-25 12:49:07 +0200

    bugfix startupscript

--- smartmirror/startup.sh
+++ smartmirror/startup.sh
@@ -5,8 +5,11 @@ here=$(dirname "$0")
 npm install $here/webserver/
 npm update $here/webserver/
 
-sh $here/../data-crawler/jvm-start.sh
-nohup python $here/../scripts/face-detect.py &
+cd $here/../stuff/data-crawler/
+sh jvm-start.sh
+
+cd $here/../stuff/scripts/
+nohup python face-detect.py &
 
 cd $here/webserver/
 nohup npm start &
