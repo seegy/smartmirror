8aff3a0a420577cf0d14acf7c480b451036a353e
Sören Gutzeit <soeren.gutzeit@gmail.com>
2018-01-13 13:27:15 +0100

    replaced some hardcoded strings by configs

--- scripts/redis_profile_creater.py
+++ scripts/redis_profile_creater.py
@@ -18,6 +18,11 @@ config = {
     'db': Config.get('Redis', 'db'),
 }
 
+profile_list = Config.get('Redis', 'profile_list')
+profile_prefix = Config.get('Redis', 'profile_prefix')
+profile_id_attr = Config.get('Redis', 'profile_id_attr')
+
+
 r = redis.StrictRedis(**config)
 
 profile_yaml = open('./conf/profiles.yml')
@@ -25,23 +30,23 @@ profiles = yaml.load(profile_yaml)
 
 for p in profiles:
 
-    if r.hmset("profile:" + str(p['faceId']), p):
+    if r.hmset(profile_prefix + ":" + str(p[profile_id_attr]), p):
         progress_dot()
 
 
-if r.sadd('profiles', *set( map( lambda p : p['faceId'], profiles))):
+if r.sadd(profile_list, *set( map( lambda p : p[profile_id_attr], profiles))):
     progress_dot()
 
 
 if r.bgsave():
     print "\nPushed and saved."
 
-profileset = r.smembers('profiles')
+profileset = r.smembers(profile_list)
 
 ks = None
 
 for pid in profileset :
-    name = 'profile:' + pid
+    name = profile_prefix + ":" + pid
 
     if not ks:
         ks = r.hkeys(name)
